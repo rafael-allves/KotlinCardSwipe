c0b051fa37da3ca144b3a36a0882dedf9cd38049
Marcos Silva <marcoseduardo_lopes_@hotmail.com>
2024-04-15 11:24:13 -0300

    Refactor CardStackManager and CardStackView classes

--- app/src/main/java/com/kotlinswipecard/CardStackManager.kt
+++ app/src/main/java/com/kotlinswipecard/CardStackManager.kt
@@ -1,4 +1,29 @@
 package com.kotlinswipecard
 
-class CardStackManager {
+import androidx.recyclerview.widget.RecyclerView
+
+class CardStackManager: RecyclerView.LayoutManager() {
+    override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams {
+        return RecyclerView.LayoutParams(
+            RecyclerView.LayoutParams.MATCH_PARENT,
+            RecyclerView.LayoutParams.MATCH_PARENT
+        )
+    }
+
+    override fun onLayoutChildren(recycler: RecyclerView.Recycler?, state: RecyclerView.State?) {
+        super.onLayoutChildren(recycler, state)
+    }
+
+    override fun canScrollHorizontally(): Boolean {
+        return true
+    }
+
+    override fun canScrollVertically(): Boolean {
+        return true
+    }
+
+    override fun scrollHorizontallyBy(dx: Int, recycler: RecyclerView.Recycler?, state: RecyclerView.State?): Int {
+        return super.scrollHorizontallyBy(dx, recycler, state)
+    }
+
 }
--- app/src/main/java/com/kotlinswipecard/CardStackView.kt
+++ app/src/main/java/com/kotlinswipecard/CardStackView.kt
@@ -1,4 +1,17 @@
 package com.kotlinswipecard
 
-class CardStackView {
+import android.content.Context
+import android.view.MotionEvent
+import androidx.recyclerview.widget.RecyclerView
+
+class CardStackView(context: Context) : RecyclerView(context) {
+
+    override fun setAdapter(adapter: Adapter<*>?) {
+        super.setAdapter(adapter)
+    }
+
+    override fun onInterceptTouchEvent(e: MotionEvent?): Boolean {
+        return super.onInterceptTouchEvent(e)
+    }
+
 }
