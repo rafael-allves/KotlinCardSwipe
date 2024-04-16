ec98e062b4b8dca0f9ca0d80081110a029addf88
rafael-allves <rafa.viana.moc@gmail.com>
2024-04-16 18:00:22 -0300

    creating animationUtil

---
+++ app/src/main/java/com/kotlinswipecard/Animator/AnimationUtil.kt
@@ -0,0 +1,21 @@
+package com.kotlinswipecard.Animator
+
+import android.animation.Animator
+
+class AnimationUtil {
+    abstract class AnimationEndListener : Animator.AnimatorListener {
+        override fun onAnimationStart(animation: Animator) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onAnimationCancel(animation: Animator) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onAnimationRepeat(animation: Animator) {
+            TODO("Not yet implemented")
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/kotlinswipecard/Animator/SwipeHelper.kt
@@ -0,0 +1,4 @@
+package com.kotlinswipecard.Animator
+
+class SwipeHelper {
+}
